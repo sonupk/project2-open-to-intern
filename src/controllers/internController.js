d11b27214f2aa3905440ad492d367da3f5ee486f
shadanayaan <b170154@nitsikkim.ac.in>
2022-09-14 12:37:04 +0530

    intern successfully

---
+++ src/controllers/internController.js
@@ -0,0 +1,22 @@
+const mongoose= require('mongoose')
+const internModel = require('../model/internModel')
+const collegeModel = require('../model/collegeModel')
+
+
+const createIntern = async function (req,res) {
+    try {
+        const internData = req.body
+
+        const { name, mobile, email, collegeName} = req.body
+        const newIntern = await internModel.create(internData)
+        res.status(201).send({status:true, message:"internship successfully created", data:newIntern})
+        
+
+    }
+    catch(err) {
+        return res.status(500).send({status: false, message:"Error" ,error:err.message})
+    }
+}
+
+module.exports.createIntern=createIntern
+
