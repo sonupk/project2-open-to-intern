be61ce7c9dd87b6c1fdb8a19a54c59c965a36971
Satish <satish_95raj@outlook.com>
2022-09-15 01:08:58 +0530

    Formatted and added some validation

--- src/controllers/collageController.js
+++ src/controllers/collageController.js
@@ -1,45 +1,44 @@
 const collageModel= require('../model/collageModel')
 
 
-
-
+//--------------------College Creation--------------------------------------
 const createCollege =async function (req,res) {
     try {
         const collegeData = req.body
+       
+        //--------------if body is empty------------------------
         if (Object.keys(collegeData).length < 1){
-            return res.status (400).send({status : false, message : "Bad request"})
+        return res.status (400).send({status : false, message : "Bad request"})
         }
-
+       
+        //-------------Abbreviation Validation-----------------
         if (!collegeData.name){
-            return res.status (400).send({status: false, message : "required name"})
+        return res.status (400).send({status: false, message : "required name"})
         }
         if (!(/^[a-zA-Z ]{3,6}$/).test(collegeData.name))
         return res.status(400).send({status:false, message:'Only alphabets in Name!!'})
 
-
+        //------------------fullName Validation--------------------
         if (!collegeData.fullName){
-            return res.status (400).send({status : false, message: "required fullName"})
+        return res.status (400).send({status : false, message: "required fullName"})
         }
         if (!(/^[a-zA-Z ,-]{5,}$/).test(collegeData.fullName))
         return res.status(400).send({status:false, message:'Only alphabets in collegeName!!'})
-
+        
+        //----------------validation for logo link----------------------------
         if (!collegeData.logoLink){
-            return res.status(400).send({status : false, msg : "required logoLink"})
+        return res.status(400).send({status : false, msg : "required logoLink"})
         }
-
-        //validation for logo link
 
         if (!(/^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/).test(collegeData.logoLink))
         return res.status(400).send({status:false, message:'Invalid logo url link !!'})
        
         
-        //checking for duplicate data
-        
+        //-----------------checking for duplicate data------------------------
         if(await collageModel.findOne({name:collegeData.name}))
-        
         return res.status(400).send({status:false, message:'Data is already present in Database'})
         
-        //creating college documents-------------
+        //--------------------creating college documents-------------------------
         const { name, fullName, logoLink } = collegeData
         const newCollege = await collageModel.create(collegeData)
         
@@ -47,9 +46,11 @@ const createCollege =async function (req,res) {
 
     }
     catch (err) {
-        // internal server error
+        // -----------------------internal server error------------------------------
         return res.status(500).send({ status: false, message: "Error", error: err.message })
     }
 }
+
+
 module.exports.createCollege=createCollege
 
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -12,44 +12,61 @@ const createIntern = async function (req,res) {
         const { name, mobile, email, collegeName} = internData
         
         if (Object.keys(internData).length < 1){
-            return res.status (400).send({status : false, message : "Bad request"})
+        return res.status (400).send({status : false, message : "Bad request"})
         }
-
+//------------------intern name validation---------------
         if (!internData.name){
-            return res.status (400).send({status: false, message : "required name"})
+        return res.status (400).send({status: false, message : "required name"})
         }
         
         if (!(/^[a-zA-Z ]{3,}$/).test(internData.name))
         return res.status(400).send({status:false, message:'Only alphabets in name!!'})
 
+//------------------intern name validation---------------
+        if (!internData.name){
+        return res.status (400).send({status: false, message : "required name"})
+        }
+        
+        if (!(/^[a-zA-Z ]{3,}$/).test(internData.name))
+        return res.status(400).send({status:false, message:'Only alphabets in name!!'})
+
+//------------------intern CollegeName validation---------------
+        if (!internData.collegeName){
+        return res.status (400).send({status: false, message : "required collegeName"})
+        }
+        
+        if (!(/^[a-zA-Z]{3,5}$/).test(internData.collegeName))
+        return res.status(400).send({status:false, message:'Only alphabets in collegeName and length between (3-5)'})
+
 
 // -----------------for email validation------------------
         if (!internData.email){
-            return res.status (400).send({status : false, message: "required email"})
+        return res.status (400).send({status : false, message: "required email"})
         }
 
         if (!validator.isEmail(internData.email.trim())){
-            return res.status(400).send({status : false, message : "required valid email"})
+        return res.status(400).send({status : false, message : "required valid email"})
         }
 
         const findEmail = await internModel.find({email : internData.email})
-        // console.log(internData.email);
+
         if (findEmail.length > 0){
-            return res.status(400).send({status : false, message :"email is aldready taken"})
+        return res.status(400).send({status : false, message :"email is aldready taken"})
         }
         
 // --------------------for phone validation----------------
         if (!internData.mobile){
-            return res.status(400).send({status : false, message : "required mobile Number"})
+        return res.status(400).send({status : false, message : "required mobile Number"})
         }
         
-        if (internData.mobile.length < 10 || internData.mobile.length >= 11){
-            return res.status(400).send({status : false, message : "mobile number is invalid"})
+        if (!(/^[0-9]{10}$/).test(internData.mobile)){
+        return res.status(400).send({status : false, message : "mobile number is invalid"})
         }
 
         const findMobile = await internModel.find({mobile : internData.mobile})
+       
         if (findMobile.length > 0){
-            return res.status(400).send({status : false, message :"mobile Number is aldready taken"})
+        return res.status(400).send({status : false, message :"mobile Number is aldready taken"})
         }
 
     //------------------collegeDetails validation-------------------- 
@@ -57,12 +74,13 @@ const createIntern = async function (req,res) {
         let collegeDetails = await collegeModel.findOne({name:collegeName})
         
         if (!collegeDetails){
-            return res.status(400).send({ status : false, message : "college details not found"})
+        return res.status(400).send({ status : false, message : "college details not found"})
         }
 
         let collegeId = collegeDetails._id
         let newData ={name,mobile,email,collegeId}
         const newIntern = await internModel.create(newData)
+        
         return res.status(201).send({status:true, message:"internship successfully created", data:newIntern})
         
     }
@@ -75,30 +93,37 @@ const createIntern = async function (req,res) {
 const geDetails = async function(req, res){
     try{    
         let name = req.query.name
-        
-        //validation for name(collgeName abberiviation)
+
+//---------------validation for name(collgeName abberiviation)----------------------------
+
         if (!name){
-            return res.status(400).send({status : false, message : "name is required"})
+        return res.status(400).send({status : false, message : "name is required"})
         }
+        
         if (!name.trim() || !(/^[a-zA-Z]{3,}$/).test(name)){
-            return res.status(400).send({status : false, message : "Enter valid abbreviation"})
+        return res.status(400).send({status : false, message : "Enter valid abbreviation"})
         }
 
         let collegeData = await collegeModel.findOne({name:name})
+
+        //-----if college not found--------
         if(!collegeData){
-            return res.status(400).send({status : false, message : "college not found"})
+        return res.status(400).send({status : false, message : "college not found"})
         }
 
         let college_id =collegeData._id
+
+        //-----if college_id not found--------
         if (!college_id){
-            return res.status(400).send({status : false, message : "college id is not found"})
+        return res.status(400).send({status : false, message : "college id is not found"})
         }
 
-        let internData =await internModel.find({collegeId:college_id}).select({name:1,email:1,mobile:1})
+        let internData =await internModel.find({collegeId:college_id, isDeleted: false}).select({name:1,email:1,mobile:1})
+        
         res.status(200).send({staus:true, name:collegeData.name,fullName:collegeData.fullName,logoLink:collegeData.logoLink,interns:internData})
         
         }catch(err){
-            return res.status(500).send({status:false, error:err.message})
+        return res.status(500).send({status:false, message:'Error', error:err.message})
         }
     }
     
