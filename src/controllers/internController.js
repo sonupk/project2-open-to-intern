16ceb0ea89b4277f7608b55f8e6812b23ca96268
Satish <satish_95raj@outlook.com>
2022-09-14 13:46:58 +0530

    getdetails

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -60,5 +60,21 @@ const createIntern = async function (req,res) {
     }
 }
 
-module.exports.createIntern=createIntern
 
+const geDetails = async function(req, res){
+    try{    
+        let name = req.query.name
+        let collegeData = await collegeModel.findOne(name)
+        let college_id =collegeData._id
+        let internData =await internModel.find({collegeId:college_id}).select({name:1,email:1,mobile:1})
+        
+        res.status(200).send({staus:true, name:collegeData.name,fullName:collegeData.fullName,logoLink:collegeData.logoLink,interns:internData})
+        
+        }catch(err){
+            res.status(400).send({status:false, error:err.message})
+        }
+    }
+    
+    
+    module.exports.geDetails=geDetails
+    module.exports.createIntern=createIntern
