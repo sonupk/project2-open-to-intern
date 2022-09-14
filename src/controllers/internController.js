7613698cdec7925348990a835e244f24846355ea
Satish <satish_95raj@outlook.com>
2022-09-14 13:03:25 +0530

    done

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -10,9 +10,16 @@ import internModel from '../model/internModel'
 
 
 const geDetails = async function(req, res){
+try{    
     let name = req.query.name
     let collegeData = await collegeModel.findOne(name)
     let college_id =collegeData._id
     let internData =await internModel.find({collegeId:college_id}).select({name:1,email:1,mobile:1})
+    
+    res.status(200).send({staus:true, name:collegeData.name,fullName:collegeData.fullName,logoLink:collegeData.logoLink,interns:internData})
+    
+    }catch(err){
+        res.status(400).send({status:false, error:err.message})
+    }
 
 }
