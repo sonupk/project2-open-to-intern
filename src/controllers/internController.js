bcb960adcd634d3bf50e7851479a398a76a93dc6
Muhaz <muhazvla313@gmail.com>
2022-09-14 13:05:10 +0530

    upD

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,25 +1,22 @@
-import collegeModel from '../model/collageModel'
-import internModel from '../model/internModel'
+const mongoose= require('mongoose')
+const internModel = require('../model/internModel')
+const collegeModel = require('../model/collageModel')
 
 
+const createIntern = async function (req,res) {
+    try {
+        const internData = req.body
 
+        const { name, mobile, email, collegeName} = req.body
+        const newIntern = await internModel.create(internData)
+        res.status(201).send({status:true, message:"internship successfully created", data:newIntern})
+        
 
-
-
-
-
-
-const geDetails = async function(req, res){
-try{    
-    let name = req.query.name
-    let collegeData = await collegeModel.findOne(name)
-    let college_id =collegeData._id
-    let internData =await internModel.find({collegeId:college_id}).select({name:1,email:1,mobile:1})
-    
-    res.status(200).send({staus:true, name:collegeData.name,fullName:collegeData.fullName,logoLink:collegeData.logoLink,interns:internData})
-    
-    }catch(err){
-        res.status(400).send({status:false, error:err.message})
     }
-
+    catch(err) {
+        return res.status(500).send({status: false, message:"Error" ,error:err.message})
+    }
 }
+
+module.exports.createIntern=createIntern
+
